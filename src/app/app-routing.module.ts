341fde0ec92e0dc05c62331797e1e5e819f9a359
Francisco <franciscoj.sparcoltary@gmail.com>
2019-09-13 20:33:25 -0500

    Creacion de pagina de bienvenida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,8 @@ const routes: Routes = [
   { path: 'comunidad', loadChildren: './pages/comunidad/comunidad.module#ComunidadPageModule' },
   { path: 'blog', loadChildren: './pages/blog/blog.module#BlogPageModule' },
   { path: 'configuracion', loadChildren: './pages/configuracion/configuracion.module#ConfiguracionPageModule' },
-  { path: 'soporte-tecnico', loadChildren: './pages/soporte-tecnico/soporte-tecnico.module#SoporteTecnicoPageModule' },
+  { path: 'soporte-tecnico', loadChildren: './pages/soporte-tecnico/soporte-tecnico.module#SoporteTecnicoPageModule' },  { path: 'bienvenido', loadChildren: './pages/bienvenido/bienvenido.module#BienvenidoPageModule' },
+
 
 
   // { path: '**', redirectTo: 'login'},
